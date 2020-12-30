4c404bda8a7f97b48b9fad861d066087677ab406
Jack <jackwchoi@pm.me>
2020-12-30 14:55:30 -0500

    tests_e2e broken up for organization

---
+++ src/tests_e2e/fail.rs
@@ -0,0 +1,578 @@
+use crate::tests_e2e::util::*;
+use crate::{fs_util::*, prelude::*, test_util::*, util::*};
+use itertools::Itertools;
+use std::{io::Write, path::PathBuf};
+
+#[test]
+fn password_confirmation_fail_during_encryption() {
+    //
+    let exit_code = CsyncErr::PasswordConfirmationFail.exit_code();
+
+    // two different keys
+    let key_1 = "oKorQhgZY3MFLRzc236NPWyghxzSf2EW";
+    let key_2 = "lWAI3d36N4g4GXtjYB8cjgNwcfkbzmYc";
+
+    //
+    let source = tmpdir!().unwrap();
+    let out_dir = tmpdir!().unwrap();
+
+    // encryption checks
+    check_core!(
+        exit_code,
+        key_1,
+        key_2,
+        path_as_str!(&source.path()),
+        &format!("-o {}", path_as_str!(out_dir.path())),
+        "-v"
+    );
+
+    //
+    assert!(dir_is_empty(out_dir.path()));
+}
+
+#[test]
+fn metadata_load_failed() {
+    //
+    let decryption_exit_code = CsyncErr::MetadataLoadFailed(String::new()).exit_code();
+
+    //
+    let source = tmpdir!().unwrap();
+    let source = source.path();
+
+    // shadow because we don't want move or drop
+    let out_dir = tmpdir!().unwrap();
+    let out_dir = out_dir.path();
+
+    // shadow because we don't want move or drop
+    let out_out_dir = tmpdir!().unwrap();
+    let out_out_dir = out_out_dir.path();
+
+    // same keys, so it shouldn't fail from mismatch
+    let key_1 = "JPSIXf4R4tQcOWe9U3paJTcKoUiEQmHm";
+    let key_2 = key_1;
+
+    // decryption checks
+    check_decrypt!(
+        decryption_exit_code,
+        &out_dir,
+        &out_out_dir,
+        source,
+        key_1,
+        key_2,
+        path_as_str!(&out_dir),
+        &format!("-o {} -d", path_as_str!(out_out_dir)),
+        "-v"
+    );
+
+    //
+    assert!(dir_is_empty(&source));
+    assert!(dir_is_empty(&out_dir));
+    assert!(dir_is_empty(&out_out_dir));
+}
+
+#[test]
+fn authentication_fail() {
+    //
+    let encryption_exit_code = 0;
+    let decryption_exit_code = CsyncErr::AuthenticationFail.exit_code();
+
+    //
+    let source = tmpdir!().unwrap();
+    let source = source.path();
+
+    // shadow because we don't want move or drop
+    let out_dir = tmpdir!().unwrap();
+    let out_dir = out_dir.path();
+
+    // shadow because we don't want move or drop
+    let out_out_dir = tmpdir!().unwrap();
+    let out_out_dir = out_out_dir.path();
+
+    // same keys, so it shouldn't fail from mismatch
+    let key_1 = "JPSIXf4R4tQcOWe9U3paJTcKoUiEQmHm";
+    let key_2 = key_1;
+
+    // encryption checks
+    check_encrypt!(
+        encryption_exit_code,
+        &source,
+        &out_dir,
+        key_1,
+        key_2,
+        path_as_str!(source),
+        &format!("-o {}", path_as_str!(out_dir)),
+        "-v"
+    );
+
+    // different key from encryption
+    let key_1 = "G4hsElnQWIY7sUPNkmkI6pT0vosQVQPv";
+    let key_2 = key_1;
+
+    // decryption checks
+    check_decrypt!(
+        decryption_exit_code,
+        &out_dir,
+        &out_out_dir,
+        source,
+        key_1,
+        key_2,
+        path_as_str!(&out_dir),
+        &format!("-o {} -d", path_as_str!(out_out_dir)),
+        "-v"
+    );
+
+    //
+    assert!(dir_is_empty(&out_out_dir));
+}
+
+mod source_does_not_exist {
+    use super::*;
+
+    #[test]
+    fn encryption_source_does_not_exist() {
+        //
+        let exit_code = CsyncErr::SourceDoesNotExist(PathBuf::from("")).exit_code();
+
+        // same keys
+        let key_1 = "wy2SuiVU1JadEM0H4G2vGHpVg1ePJFf6";
+        let key_2 = key_1;
+
+        //
+        let tmpd = tmpdir!().unwrap();
+        let source = tmpd.path().join("aW7aK7lBgW4gZWoH3Y4LCcIn2xAgEm2d");
+        assert!(!source.exists());
+
+        //
+        let out_dir = tmpdir!().unwrap();
+
+        // encryption checks
+        check_core!(
+            exit_code,
+            key_1,
+            key_2,
+            path_as_str!(&source),
+            &format!("-o {}", path_as_str!(out_dir.path())),
+            "-v"
+        );
+
+        //
+        assert!(dir_is_empty(out_dir.path()));
+    }
+
+    #[test]
+    fn decryption_source_does_not_exist() {
+        //
+        let encryption_exit_code = 0;
+        let decryption_exit_code = CsyncErr::SourceDoesNotExist(PathBuf::from("")).exit_code();
+
+        let source = tmpdir!().unwrap();
+        let source = source.path();
+
+        // shadow because we don't want move or drop
+        let out_dir = tmpdir!().unwrap();
+        let out_dir = out_dir.path();
+
+        // shadow because we don't want move or drop
+        let out_out_dir = tmpdir!().unwrap();
+        let out_out_dir = out_out_dir.path();
+
+        // same keys, so it shouldn't fail from mismatch
+        let key_1 = "lh7DsqnJtEllR407yN2Qp9MaWaTpkTO2";
+        let key_2 = key_1;
+
+        // encryption checks
+        check_encrypt!(
+            encryption_exit_code,
+            source,
+            out_dir,
+            key_1,
+            key_2,
+            path_as_str!(source),
+            &format!("-o {}", path_as_str!(out_dir)),
+            "-v"
+        );
+
+        let dne_out_dir = tmpdir!().unwrap();
+        let dne_out_dir = dne_out_dir.path().join("grEx4pLK5p6OnrAST9GaHfCTwsc3jHQ3");
+
+        //
+        assert!(!dne_out_dir.exists());
+
+        // decryption checks
+        check_decrypt!(
+            decryption_exit_code,
+            &dne_out_dir,
+            out_out_dir,
+            source,
+            key_1,
+            key_2,
+            path_as_str!(&dne_out_dir),
+            &format!("-o {} -d", path_as_str!(out_out_dir)),
+            "-v"
+        );
+
+        //
+        assert!(dir_is_empty(&out_out_dir));
+    }
+}
+
+#[test]
+fn decryption_outdir_is_nonempty() {
+    //
+    let encryption_exit_code = 0;
+    let decryption_exit_code = CsyncErr::DecryptionOutdirIsNonempty(PathBuf::from("")).exit_code();
+
+    //
+    let key_1 = "Ig0BOcmwiUMHUv7W5kcLFvxK3iYMEF5i";
+    let key_2 = key_1;
+
+    //
+    let source = tmpdir!().unwrap();
+    let out_dir = tmpdir!().unwrap();
+    let out_out_dir = tmpdir!().unwrap();
+
+    // encryption checks
+    check_encrypt!(
+        encryption_exit_code,
+        &source,
+        out_dir.path(),
+        key_1,
+        key_2,
+        path_as_str!(&source),
+        &format!("-o {}", path_as_str!(out_dir.path())),
+        "-v"
+    );
+
+    //
+    let rand_file_pbuf = out_out_dir.path().join("HH4aOlvbxe0IX6wlokdyWfnsZULhrasj");
+    {
+        fopen_w(&rand_file_pbuf).unwrap();
+    }
+    assert!(rand_file_pbuf.exists());
+
+    //
+    check_core!(
+        decryption_exit_code,
+        key_1,
+        key_2,
+        path_as_str!(out_dir.path()),
+        &format!("-o {}", path_as_str!(out_out_dir.path())),
+        "-v -d"
+    );
+}
+
+/*
+AuthenticationFail,                    // checksum verification failed for this file
+DecryptionOutdirIsNonempty(PathBuf),   // when decrypting, outdir must be empty
+HashSpecConflict,                      //
+IncrementalEncryptionDisabledForNow => 100,
+InvalidSpreadDepth(SpreadDepth),       // spread depth is outside of the allowed range
+MetadataLoadFailed(String),            // couldn't load this metadata file
+NonFatalReportFailed,                  //
+Other(String),                         // anything else
+OutdirIsNotDir(PathBuf),               // ...  decrypting ...
+PasswordConfirmationFail,              //
+PathContainsInvalidUtf8Bytes(PathBuf), //
+SerdeFailed,                           //
+SourceDoesNotExist(PathBuf),           //
+SourceDoesNotHaveFilename(PathBuf),    //
+SourceEqOutdir(PathBuf),               //
+
+non_fatal_report_failed
+other
+path_contains_invalid_utf8_bytes
+serde_failed
+*/
+
+// TODO test to see what happens if outdir a symlink
+
+#[test]
+fn outdir_is_not_dir() {
+    //
+    let exit_code = CsyncErr::OutdirIsNotDir(PathBuf::from("")).exit_code();
+
+    // same keys
+    let key_1 = "LeVkTH1hGLyYQM4MrGabWbh1STb4IS2P";
+    let key_2 = key_1;
+
+    //
+    let source = tmpdir!().unwrap();
+
+    //
+    let tmpd = tmpdir!().unwrap();
+    let out_dir = tmpd.path().join("emnzmttCuAiVsrsSTerxHeTcU2ebOWqE");
+    {
+        fopen_w(&out_dir).unwrap();
+    }
+    assert!(out_dir.exists());
+    assert!(out_dir.is_file());
+
+    // encryption checks
+    check_core!(
+        exit_code,
+        key_1,
+        key_2,
+        path_as_str!(&source),
+        &format!("-o {}", path_as_str!(&out_dir)),
+        "-v"
+    );
+}
+
+#[test]
+fn source_eq_outdir() {
+    //
+    let exit_code = CsyncErr::SourceEqOutdir(PathBuf::from("")).exit_code();
+
+    // same keys
+    let key_1 = "f4NKSepEHuDJ5Ja8Nm9D1MatBOn97YgB";
+    let key_2 = key_1;
+
+    //
+    let source = tmpdir!().unwrap();
+
+    // encryption checks
+    check_core!(
+        exit_code,
+        key_1,
+        key_2,
+        path_as_str!(&source.path()),
+        &format!("-o {}", path_as_str!(&source.path())),
+        "-v"
+    );
+}
+
+#[test]
+fn source_does_not_have_filename() {
+    //
+    let exit_code = CsyncErr::SourceDoesNotHaveFilename(PathBuf::from("")).exit_code();
+
+    // same keys
+    let key_1 = "TOCX1X58BkZDezq2GSN0hVK0jB4kxj04";
+    let key_2 = key_1;
+
+    //
+    let source = PathBuf::from("/");
+
+    //
+    let out_dir = tmpdir!().unwrap();
+
+    // encryption checks
+    check_core!(
+        exit_code,
+        key_1,
+        key_2,
+        path_as_str!(&source),
+        &format!("-o {}", path_as_str!(&out_dir.path())),
+        "-v"
+    );
+}
+
+mod command_line_argument_conflict {
+    use super::*;
+
+    //
+    macro_rules! testgen {
+            //
+            ( $fn_name:ident, $( $arg:expr ),+ ) => {
+                //
+                #[test]
+                fn $fn_name() {
+                    //
+                    let exit_code = CsyncErr::CommandLineArgumentConflict(String::new()).exit_code();
+
+                    // same keys
+                    let key_1 = "aqMnSbXfScceMjJL5PCC7pHQ2ABeo6YF";
+                    let key_2 = key_1;
+
+                    //
+                    let source = tmpdir!().unwrap();
+                    let out_dir = tmpdir!().unwrap();
+
+                    // encryption checks
+                    check_core!(
+                        exit_code,
+                        key_1,
+                        key_2,
+                        path_as_str!(&source),
+                        &format!("-o {}", path_as_str!(&out_dir)),
+                        "-v"
+                        $( , $arg )+
+                    );
+                }
+            };
+        }
+
+    testgen!(clean_and_decrypt, "--clean", "--decrypt");
+}
+
+mod hash_spec_conflict {
+    use super::*;
+
+    //
+    macro_rules! testgen {
+            //
+            ( $fn_name:ident, $( $arg:expr ),+ ) => {
+                //
+                #[test]
+                fn $fn_name() {
+                    //
+                    let exit_code = CsyncErr::HashSpecConflict.exit_code();
+
+                    // same keys
+                    let key_1 = "LeVkTH1hGLyYQM4MrGabWbh1STb4IS2P";
+                    let key_2 = key_1;
+
+                    //
+                    let source = tmpdir!().unwrap();
+                    let out_dir = tmpdir!().unwrap();
+
+                    // encryption checks
+                    check_core!(
+                        exit_code,
+                        key_1,
+                        key_2,
+                        path_as_str!(&source),
+                        &format!("-o {}", path_as_str!(&out_dir)),
+                        "-v"
+                        $( , $arg )+
+                    );
+                }
+            };
+        }
+
+    //
+    testgen!(pbkdf2_conflict, "--pbkdf2-num-iter 142390", "--pbkdf2-time 2");
+
+    // conflicts within scrypt
+    mod scrypt {
+        use super::*;
+
+        testgen!(time_logn, "--scrypt-time 3", "--scrypt-log-n 9");
+        testgen!(time_r, "--scrypt-time 4", "--scrypt-r 4");
+        testgen!(time_p, "--scrypt-time 5", "--scrypt-p 2");
+        testgen!(time_r_p, "--scrypt-time 6", "--scrypt-r 3", "--scrypt-p 2");
+        testgen!(time_logn_r, "--scrypt-time 6", "--scrypt-log-n 9", "--scrypt-r 3");
+        testgen!(time_logn_p, "--scrypt-time 6", "--scrypt-log-n 9", "--scrypt-p 2");
+        testgen!(
+            time_r_p_logn,
+            "--scrypt-time 7",
+            "--scrypt-log-n 10",
+            "--scrypt-r 3",
+            "--scrypt-p 2"
+        );
+    }
+
+    // conflicts between pbkdf2 and scrypt
+    mod pbkdf2_scrypt {
+        use super::*;
+
+        testgen!(pbkdf2_time_scrypt_time, "--pbkdf2-time 2", "--scrypt-time 1");
+        testgen!(pbkdf2_time_scrypt_logn, "--pbkdf2-time 2", "--scrypt-log-n 9");
+        testgen!(pbkdf2_time_scrypt_r, "--pbkdf2-time 2", "--scrypt-r 4");
+        testgen!(pbkdf2_time_scrypt_p, "--pbkdf2-time 2", "--scrypt-p 2");
+
+        testgen!(pbkdf2_numiter_scrypt_time, "--pbkdf2-num-iter 141414", "--scrypt-time 2");
+        testgen!(pbkdf2_numiter_scrypt_logn, "--pbkdf2-num-iter 123123", "--scrypt-log-n 9");
+        testgen!(pbkdf2_numiter_scrypt_r, "--pbkdf2-num-iter 1423122", "--scrypt-r 4");
+        testgen!(pbkdf2_numiter_scrypt_p, "--pbkdf2-num-iter 1978239", "--scrypt-p 2");
+    }
+}
+
+mod invalid_spread_depth {
+    use super::*;
+
+    //
+    macro_rules! testgen {
+            //
+            ( $fn_name:ident, $( $arg:expr ),+ ) => {
+                //
+                #[test]
+                fn $fn_name() {
+                    //
+                    let exit_code = CsyncErr::InvalidSpreadDepth(43).exit_code();
+
+                    // same keys
+                    let key_1 = "wsJlckEFNdxH5v7Z10pulyOzbglPOfAH";
+                    let key_2 = key_1;
+
+                    //
+                    let source = tmpdir!().unwrap();
+                    let out_dir = tmpdir!().unwrap();
+
+                    // encryption checks
+                    check_core!(
+                        exit_code,
+                        key_1,
+                        key_2,
+                        path_as_str!(&source),
+                        &format!("-o {}", path_as_str!(&out_dir)),
+                        "-v"
+                        $( , $arg )+
+                    );
+                }
+            };
+        }
+
+    testgen!(invalid_0, "--spread-depth 0");
+
+    testgen!(invalid_256, "--spread-depth 256");
+
+    testgen!(invalid_257, "--spread-depth 257");
+
+    testgen!(invalid_12345, "--spread-depth 12345");
+}
+
+mod incremental_encryption_disabled_for_now {
+    use super::*;
+
+    //
+    macro_rules! testgen {
+        //
+        ( $fn_name:ident, $outdir_and_tmpd:expr ) => {
+            //
+            #[test]
+            fn $fn_name() {
+                //
+                let exit_code = CsyncErr::IncrementalEncryptionDisabledForNow.exit_code();
+
+                // same keys
+                let key_1 = "s5cZP4BNq0LlcWzlPG8vxho569u7d120";
+                let key_2 = key_1;
+
+                //
+                let source = tmpdir!().unwrap();
+                let (out_dir, _tmpd) = $outdir_and_tmpd;
+
+                // encryption checks
+                check_core!(
+                    exit_code,
+                    key_1,
+                    key_2,
+                    path_as_str!(&source),
+                    &format!("-o {}", path_as_str!(&out_dir)),
+                    "-v"
+                );
+            }
+        };
+    }
+
+    testgen!(contains_file, {
+        let tmpd = tmpdir!().unwrap();
+
+        let filepath = tmpd.path().join("4Lhfo56kkktP95PYfXFWc5JNmRT8iCVj");
+        {
+            std::fs::File::create(&filepath).unwrap();
+        }
+
+        (tmpd.path().to_path_buf(), tmpd)
+    });
+
+    testgen!(contains_empty_dir, {
+        let tmpd = tmpdir!().unwrap();
+
+        let dirpath = tmpd.path().join("4Lhfo56kkktP95PYfXFWc5JNmRT8iCVj");
+        std::fs::create_dir(&dirpath).unwrap();
+
+        (tmpd.path().to_path_buf(), tmpd)
+    });
+}
---
+++ src/tests_e2e/success.rs
@@ -0,0 +1,190 @@
+use crate::tests_e2e::util::*;
+use crate::{fs_util::*, prelude::*, test_util::*, util::*};
+use itertools::Itertools;
+use std::{io::Write, path::PathBuf};
+use tempfile::TempDir;
+
+#[test]
+fn encrypted_dir_basename_changed() {
+    let source = tmpdir!().unwrap();
+    let source = source.path();
+
+    // pass
+    let exit_code = 0;
+
+    //
+    let tmpd = tmpdir!().unwrap();
+
+    //
+    let out_dir = tmpd.path().join("auVj2ZazQDC0ZNvgAp7WyhIfR0PSTyJS");
+    std::fs::create_dir(&out_dir).unwrap();
+    //
+    let renamed_out_dir = tmpd.path().join("jhlqRjoF7Iy3xA8TKtCpxiCr6YdH2cMC");
+
+    //
+    let out_out_dir = tmpdir!().unwrap();
+    let out_out_dir = out_out_dir.path();
+
+    // same keys, so it shouldn't fail from mismatch
+    let key_1 = "NbhfRifWQdHyUHPTrK0joRJ7u1NvsGL1";
+    let key_2 = key_1;
+
+    // encryption checks
+    check_encrypt!(
+        exit_code,
+        &source,
+        &out_dir,
+        key_1,
+        key_2,
+        path_as_str!(&source),
+        &format!("-o {}", path_as_str!(&out_dir)),
+        "-v"
+    );
+
+    std::fs::rename(&out_dir, &renamed_out_dir).unwrap();
+
+    // decryption checks
+    check_decrypt!(
+        exit_code,
+        &renamed_out_dir,
+        &out_out_dir,
+        &source,
+        key_1,
+        key_2,
+        path_as_str!(&renamed_out_dir),
+        &format!("-o {} -d", path_as_str!(&out_out_dir)),
+        "-v"
+    );
+}
+
+//
+macro_rules! generate_success_body {
+        //
+        ( $fn_name:ident, $pbuf_and_tmpd:expr, $key:literal $(, $arg:literal )* ) => {
+            //
+            #[test]
+            fn $fn_name() {
+                let (source, _tmpd): (PathBuf, Option<TempDir>) = $pbuf_and_tmpd;
+                let source = &source;
+
+                // pass
+                let exit_code = 0;
+
+                // shadow because we don't want move or drop
+                let out_dir = tmpdir!().unwrap();
+                let out_dir = out_dir.path();
+
+                // shadow because we don't want move or drop
+                let out_out_dir = tmpdir!().unwrap();
+                let out_out_dir = out_out_dir.path();
+
+                // same keys, so it shouldn't fail from mismatch
+                let key_1 = "0LJov4GYGMPUvzQSuxcap3guApyhB3KM";
+                let key_2 = key_1;
+
+                // encryption checks
+                check_encrypt!(
+                    exit_code,
+                    source,
+                    out_dir,
+                    key_1,
+                    key_2,
+                    path_as_str!(source),
+                    &format!("-o {}", path_as_str!(out_dir)),
+                    "-v"
+                    $(, $arg )*
+                );
+
+                // decryption checks
+                check_decrypt!(
+                    exit_code,
+                    out_dir,
+                    out_out_dir,
+                    source,
+                    key_1,
+                    key_2,
+                    path_as_str!(out_dir),
+                    &format!("-o {} -d", path_as_str!(out_out_dir)),
+                    "-v"
+                );
+            }
+        }
+    }
+
+// # Parameters
+//
+// 1. `$mod_name`
+// 1. `$key`
+// 1. `$( , $arg )*`: string args to pass to the csync prorcess
+macro_rules! generate_mod {
+        //
+        ( $mod_name:ident, $key:literal $( , $arg:literal )* ) => {
+            //
+            mod $mod_name {
+                use super::*;
+
+                //
+                macro_rules! generate_test {
+                    ( $fn_name:ident, $pbuf_and_tmpd:expr ) => {
+                        generate_success_body!(
+                            $fn_name,
+                            $pbuf_and_tmpd,
+                            $key
+                            $( , $arg )*
+                        );
+                    };
+                }
+
+                //
+                generate_suite!(generate_test);
+            }
+        }
+    }
+
+// 1. default configs
+// 1. random password
+generate_mod!(default, "08h4eMP5jWCtm09PWFMEK8ND6nAxfv1NrztA4S1t0wFhi3NmRlbFis4ERFyCcKmL");
+
+// 1. default configs
+// 1. empty password
+generate_mod!(default_empty_password, "");
+
+// 1. `aes256cbc` as the cipher
+// 1. hash strength specified by number of iteration
+generate_mod!(
+    aes256cbc_pbkdf2_params,
+    "H7u1ZPOHnzMVXyVT6vBSnkIe6TMTj5otNQGi2wmIW4lJw36sBccg8PCm5AKX8iMX",
+    "--cipher aes256cbc",
+    "--spread-depth 4",
+    "--pbkdf2-num-iter 89432"
+);
+
+// 1. `aes256cbc` as the cipher
+// 1. hash strength specified by time
+generate_mod!(
+    aes256cbc_pbkdf2_time,
+    "H7u1ZPOHnzMVXyVT6vBSnkIe6TMTj5otNQGi2wmIW4lJw36sBccg8PCm5AKX8iMX",
+    "--cipher aes256cbc",
+    "--spread-depth 5",
+    "--pbkdf2-algorithm hmac-sha512",
+    "--pbkdf2-time 4"
+);
+
+generate_mod!(
+    chacha20_scrypt_params_custom_len,
+    "H7u1ZPOHnzMVXyVT6vBSnkIe6TMTj5otNQGi2wmIW4lJw36sBccg8PCm5AKX8iMX",
+    "--cipher chacha20",
+    "--spread-depth 6",
+    "--scrypt-time 3",
+    "--scrypt-output-len 1483"
+);
+
+generate_mod!(
+    chacha20_scrypt_time,
+    "H7u1ZPOHnzMVXyVT6vBSnkIe6TMTj5otNQGi2wmIW4lJw36sBccg8PCm5AKX8iMX",
+    "--cipher chacha20",
+    "--spread-depth 7",
+    "--scrypt-log-n 13",
+    "--scrypt-r 9",
+    "--scrypt-p 2"
+);
