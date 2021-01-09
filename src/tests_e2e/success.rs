b9d38841867d5be5b3b92e75e7ea32f9de190a3f
Jack <jackwchoi@pm.me>
2021-01-09 15:34:01 -0500

    quick refactor

--- src/tests_e2e/success.rs
+++ src/tests_e2e/success.rs
@@ -57,55 +57,55 @@ fn encrypted_dir_basename_changed() {
 
 //
 macro_rules! generate_success_body {
+    //
+    ( $fn_name:ident, $pbuf_and_tmpd:expr, $key:literal $(, $arg:literal )* ) => {
         //
-        ( $fn_name:ident, $pbuf_and_tmpd:expr, $key:literal $(, $arg:literal )* ) => {
-            //
-            #[test]
-            fn $fn_name() {
-                let (source, _tmpd): (PathBuf, Option<TempDir>) = $pbuf_and_tmpd;
-                let source = &source;
+        #[test]
+        fn $fn_name() {
+            let (source, _tmpd): (PathBuf, Option<TempDir>) = $pbuf_and_tmpd;
+            let source = &source;
 
-                // pass
-                let exit_code = 0;
+            // pass
+            let exit_code = 0;
 
-                // shadow because we don't want move or drop
-                let out_dir = tmpdir!().unwrap();
-                let out_dir = out_dir.path();
+            // shadow because we don't want move or drop
+            let out_dir = tmpdir!().unwrap();
+            let out_dir = out_dir.path();
 
-                // shadow because we don't want move or drop
-                let out_out_dir = tmpdir!().unwrap();
-                let out_out_dir = out_out_dir.path();
+            // shadow because we don't want move or drop
+            let out_out_dir = tmpdir!().unwrap();
+            let out_out_dir = out_out_dir.path();
 
-                // same keys, so it shouldn't fail from mismatch
-                let key_1 = "0LJov4GYGMPUvzQSuxcap3guApyhB3KM";
-                let key_2 = key_1;
+            // same keys, so it shouldn't fail from mismatch
+            let key_1 = "0LJov4GYGMPUvzQSuxcap3guApyhB3KM";
+            let key_2 = key_1;
 
-                // encryption checks
-                check_encrypt!(
-                    exit_code,
-                    source,
-                    out_dir,
-                    key_1,
-                    key_2,
-                    path_as_str!(source),
-                    &format!("-o {}", path_as_str!(out_dir))
-                    $(, $arg )*
-                );
+            // encryption checks
+            check_encrypt!(
+                exit_code,
+                source,
+                out_dir,
+                key_1,
+                key_2,
+                path_as_str!(source),
+                &format!("-o {}", path_as_str!(out_dir))
+                $(, $arg )*
+            );
 
-                // decryption checks
-                check_decrypt!(
-                    exit_code,
-                    out_dir,
-                    out_out_dir,
-                    source,
-                    key_1,
-                    key_2,
-                    path_as_str!(out_dir),
-                    &format!("-o {}", path_as_str!(out_out_dir))
-                );
-            }
+            // decryption checks
+            check_decrypt!(
+                exit_code,
+                out_dir,
+                out_out_dir,
+                source,
+                key_1,
+                key_2,
+                path_as_str!(out_dir),
+                &format!("-o {}", path_as_str!(out_out_dir))
+            );
         }
     }
+}
 
 // # Parameters
 //
@@ -113,29 +113,29 @@ macro_rules! generate_success_body {
 // 1. `$key`
 // 1. `$( , $arg )*`: string args to pass to the csync prorcess
 macro_rules! generate_mod {
+    //
+    ( $mod_name:ident, $key:literal $( , $arg:literal )* ) => {
         //
-        ( $mod_name:ident, $key:literal $( , $arg:literal )* ) => {
+        mod $mod_name {
+            use super::*;
+
             //
-            mod $mod_name {
-                use super::*;
-
-                //
-                macro_rules! generate_test {
-                    ( $fn_name:ident, $pbuf_and_tmpd:expr ) => {
-                        generate_success_body!(
-                            $fn_name,
-                            $pbuf_and_tmpd,
-                            $key
-                            $( , $arg )*
-                        );
-                    };
-                }
-
-                //
-                generate_suite!(generate_test);
+            macro_rules! generate_test {
+                ( $fn_name:ident, $pbuf_and_tmpd:expr ) => {
+                    generate_success_body!(
+                        $fn_name,
+                        $pbuf_and_tmpd,
+                        $key
+                        $( , $arg )*
+                    );
+                };
             }
+
+            //
+            generate_suite!(generate_test);
         }
     }
+}
 
 // 1. default configs
 // 1. random password
@@ -149,7 +149,7 @@ generate_mod!(default_empty_password, "");
 // 1. hash strength specified by number of iteration
 generate_mod!(
     aes256cbc_pbkdf2_params,
-    "H7u1ZPOHnzMVXyVT6vBSnkIe6TMTj5otNQGi2wmIW4lJw36sBccg8PCm5AKX8iMX",
+    "CL9OhnSRp5uOeb1sZWjMulidwLmbFmL89TDo6FQ5vIq325tPiCEDQxzcK9aFC8B9",
     "--cipher aes256cbc",
     "--spread-depth 4",
     "--key-deriv-alg pbkdf2",
@@ -160,7 +160,7 @@ generate_mod!(
 // 1. hash strength specified by time
 generate_mod!(
     aes256cbc_pbkdf2_time,
-    "H7u1ZPOHnzMVXyVT6vBSnkIe6TMTj5otNQGi2wmIW4lJw36sBccg8PCm5AKX8iMX",
+    "zVzfYb4RlAdS8zng8gX7Dq1zADhOEnBxoqk4iwsmKW6oNs7A2dVPOBu9QZeXLU4c",
     "--cipher aes256cbc",
     "--spread-depth 5",
     "--key-deriv-alg pbkdf2",
@@ -170,7 +170,7 @@ generate_mod!(
 
 generate_mod!(
     chacha20_scrypt_params_custom_len,
-    "H7u1ZPOHnzMVXyVT6vBSnkIe6TMTj5otNQGi2wmIW4lJw36sBccg8PCm5AKX8iMX",
+    "AMrSoKIyDByT1kn398swxJOPUYu58b5M98BISjqcvlzpDeKtnFPOD3wULCgDZVHE",
     "--cipher chacha20",
     "--spread-depth 6",
     "--key-deriv-time 3",
@@ -179,7 +179,7 @@ generate_mod!(
 
 generate_mod!(
     chacha20_scrypt_time,
-    "H7u1ZPOHnzMVXyVT6vBSnkIe6TMTj5otNQGi2wmIW4lJw36sBccg8PCm5AKX8iMX",
+    "nTn3RoJEVvX8IH5zQZ5LKTJTSBU3ZqsTG9d2TSL2GB1DbTlNzBBaXwPxEu9DRsby",
     "--cipher chacha20",
     "--spread-depth 7",
     "--scrypt-log-n 13",
